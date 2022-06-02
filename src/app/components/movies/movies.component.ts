3eccc617eb74c78491723e4f4f1bacc6805b021d
Tomas Ruiz Diaz <tomiruizdiaz@gmail.com>
2022-06-02 14:43:35 -0300

    app finalizada

--- src/app/components/movies/movies.component.ts
+++ src/app/components/movies/movies.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { debounceTime, distinct, filter, fromEvent, map, Subscription, switchMap, tap } from 'rxjs';
+import { Movie } from 'src/app/interfaces/movies';
 import { MovieService } from 'src/app/services/movie.service';
 
 @Component({
@@ -6,21 +8,29 @@ import { MovieService } from 'src/app/services/movie.service';
   templateUrl: './movies.component.html',
   styleUrls: ['./movies.component.css']
 })
-export class MoviesComponent implements OnInit {
-  movies:any[] = [];
+export class MoviesComponent implements OnInit, OnDestroy {
+  movies:Movie[] = [];
+  @ViewChild('movieSearchInput', { static: true }) movieSearchInput!: ElementRef
+  movieSuscription!: Subscription
 
   constructor(private movieService: MovieService) { }
 
   ngOnInit(): void {
+   this.movieSuscription = fromEvent<Event>(this.movieSearchInput.nativeElement, 'keyup').pipe(
+      map((event: Event) => {
+        const searchTerm = (event.target as HTMLInputElement).value;
+        return searchTerm
+      }),
+      filter((searchTerm: string) => searchTerm.length > 3),
+      debounceTime(500),
+      distinct(),
+      switchMap((searchTerm: string) => this.movieService.getMovies(searchTerm) )
+      ).subscribe((movies: Movie[]) => {
+        this.movies = movies !== undefined ? movies : [];
+      })
   }
 
-  getMovies(event: Event) {
-    const searchTerm = (event.target as HTMLInputElement).value;
-    console.log(searchTerm);
-    this.movieService.getMovies(searchTerm).subscribe(movies => {
-      console.log(movies);
-      this.movies = movies !== undefined ? movies : [];
-    })
+  ngOnDestroy(): void {
+    this.movieSuscription.unsubscribe()
   }
-
 }
